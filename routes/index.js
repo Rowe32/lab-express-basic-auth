8424258d19436c16e315d2d0282945892dd127e3
Rowena Schmidt <schmidt.rowena@gmail.com>
2022-03-16 15:17:27 +0100

    part of iteration 1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
+const bcrypt = require("bcryptjs"); 
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+router.post("/signup", async (req, res, next) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+
+  const salt = await bycryptjs.genSalt(12);
+  const hash = await bcryptjs.hash(password, salt);
+
+  const user = {
+    username,
+    password: hash,
+  }
+  //after import user model
+  await User.create(user);
+  res.render('profile', {}) //send user data to profile welcome
+
 });
 
 module.exports = router;
